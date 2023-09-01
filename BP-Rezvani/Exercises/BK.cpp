d2553ed23ae1d107ffe22ea5577c013ad8e863d3
RamtinKosari <ramtinkosari@gmail.com>
2023-09-01 11:47:21 +0330

    Add BK*.cpp #1

---
+++ BP-Rezvani/Exercises/BK.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int r,BMM,KMM,mult;
+	cout<<"\n\033[1;33mavalin adad :\t\033[0m";
+	cin>>BMM;
+	cout<<"\n\033[1;33mdovomin adad :\t\033[0m";
+	cin>>KMM;
+	mult = BMM * KMM;
+	while (r!=0) 
+	{
+		r = BMM % KMM;
+		BMM = KMM;
+		KMM = r;
+    }
+	KMM = mult / BMM;
+	cout<<"\n\033[1;36mBMM :\t\033[0m"<<BMM;
+	cout<<"\n\033[1;36mKMM :\t\033[0m"<<KMM;
+	cin.ignore();
+}
